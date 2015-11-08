0b4f25ebd87bb70a2b10672bc03c3a46986fb56c
AlessandroSpallina <alessandrospallina1@gmail.com>
2015-11-08 21:45:51 +0100

    fix

--- src/main/gui/CLI - CommandLine/cli.c
+++ src/main/gui/CLI - CommandLine/cli.c
@@ -1,3 +1,21 @@
+/****************************************************************************
+* Copyright © 2015 Alessandro Spallina
+* email: alessandrospallina1@gmail.com
+* github: https://github.com/AlessandroSpallina
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -7,10 +25,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "ipc.h"
+#include "miscellaneous.h"
 
 void print_menu()
 {
-  printf("1) get command list\na) send command\n0) exit\n");
+  printf("a) send command\n0) exit\n");
 }
 
 int main(int argc, char *argv[])
@@ -26,17 +45,27 @@ int main(int argc, char *argv[])
     scanf("%c", &buf);
 
     switch(buf) {
-      case '1':
-
-        //write(fd, (void *)bufstr, sizeof(bufstr));
-        break;
 
       case 'a':
         printf("type the command: ");
         scanf("%s", bufstr);
-        write(fd, (void *)bufstr, sizeof(bufstr));
-        read(fd, (void *)bufstr, sizeof(bufstr));
-        printf("@ received: %s\n", bufstr);
+        
+        if(strncmp("start", bufstr, 5)==0) {
+        	write(fd, (void *)bufstr, sizeof(bufstr));
+        	read(fd, (void *)bufstr, sizeof(bufstr));
+        	if(strcmp("success", bufstr)==0) {
+        		//sto startando un modulo-thread -> apro un nuovo socket dedicato
+        		//in modo tale da avere diversi fd per diversi output da jasm
+        	} else {
+        		fprintf(stderr, "start new module fail\n");
+        	}
+        
+        
+        } else {
+        	write(fd, (void *)bufstr, sizeof(bufstr));
+        	read(fd, (void *)bufstr, sizeof(bufstr));
+        	printf("@ received: %s\n", bufstr);
+        }
         break;
 
       case '0':
@@ -44,6 +73,10 @@ int main(int argc, char *argv[])
         printf("bye!\n");
         exit(0);
         break;
+        
+      default:
+      	fprintf(stderr, "wrong option\n");
+      	break;
     }
   }
 
