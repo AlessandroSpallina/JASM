9b574fd060bf96e7fcff7a0dbcdf347c10eb402f
StefanoBelli <sa.belli@hotmail.it>
2015-12-15 07:53:01 +0100

    initializing configfile

---
+++ src/main/jasm/core/configfile.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+char * get_property_value(char * __prop)
+{
+    return "null";
+}
+
+void set_property_value(void)
+{
+    return;
+}
---
+++ src/main/jasm/core/configfile.h
@@ -0,0 +1,6 @@
+#ifndef CONFIGFILE_H
+#define CONFIGFILE_H
+
+extern void set_property_value(void);
+
+#endif // CONFIGFILE_H
