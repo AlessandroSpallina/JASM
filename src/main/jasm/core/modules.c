1c2bcdd80ed190652efc8164af8ea419eee71661
piFra <pifrancesco19@gmail.com>
2015-12-27 12:03:44 -0500

    Check for write() return value and handle errors

--- src/main/jasm/core/modules.c
+++ src/main/jasm/core/modules.c
@@ -1,21 +1,21 @@
 /****************************************************************************
-* Copyright © 2015 Alessandro Spallina
-* email: alessandrospallina1@gmail.com
-* github: https://github.com/AlessandroSpallina
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
+ * Copyright © 2015 Alessandro Spallina
+ * email: alessandrospallina1@gmail.com
+ * github: https://github.com/AlessandroSpallina
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -32,16 +32,29 @@ void getModule (int fd)
 {
     int count = 0;
     int nmodule = NMODULE;
+    ssize_t ret_val = 0;
 
-    write (fd, &nmodule, sizeof (nmodule) );
+    ret_val = write (fd, &nmodule, sizeof (nmodule) );
+    if(ret_val == 0 || ret_val == -1)
+    {
+        fprintf(stderr, "Error in the write() operation on fd");
+    }
 
     for (int i = 0; i < NMODULE; i++)
     {
         count = strlen (moduleName[i]);
 
         //aggiungere check errore nell'invio
-        write (fd, &count, sizeof (count) );
-        write (fd, moduleName[i], strlen (moduleName[i]) );
+        ret_val = write (fd, &count, sizeof (count) );
+        if(ret_val == 0 || ret_val == -1)
+        {
+            fprintf(stderr, "Error in the write() operation on fd");
+        }
+        ret_val = write (fd, moduleName[i], strlen (moduleName[i]) );
+        if(ret_val == 0 || ret_val == -1)
+        {
+            fprintf(stderr, "Error in the write() operation on fd");
+        }
     }
 
 }
