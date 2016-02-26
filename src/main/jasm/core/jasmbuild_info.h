681193cf6afa8ad5e47f1bb90ef53f17783b2cfc
StefanoBelli <sa.belli@hotmail.it>
2016-02-26 20:11:07 +0100

    Clean tree

--- src/main/jasm/core/jasmbuild_info.h
+++ src/main/jasm/core/jasmbuild_info.h
@@ -1,3 +1,3 @@
 #define VERSION "0.0.1:0-develstage-release"
-#define BUILD_DATE_CORE "2016/02/26 20:03:09"
+#define BUILD_DATE_CORE "2016/02/26 20:09:24"
 #define COPYRIGHT "GNU Public License 3 - GPLv3"
