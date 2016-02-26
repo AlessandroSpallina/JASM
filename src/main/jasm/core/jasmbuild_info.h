53d666b4278ded324102d6fb8d0530d80913509e
StefanoBelli <sa.belli@hotmail.it>
2016-02-26 20:07:42 +0100

    CMakeLists.txt => install rules, debian package building

--- src/main/jasm/core/jasmbuild_info.h
+++ src/main/jasm/core/jasmbuild_info.h
@@ -1,3 +1,3 @@
 #define VERSION "0.0.1:0-develstage-release"
-#define BUILD_DATE_CORE "2016/02/24 23:04:24"
+#define BUILD_DATE_CORE "2016/02/26 20:03:09"
 #define COPYRIGHT "GNU Public License 3 - GPLv3"
