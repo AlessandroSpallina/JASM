b779e5277222feb56427e9fdf82cd82e40c98770
StefanoBelli <sa.belli@hotmail.it>
2016-03-14 21:30:08 +0100

    Updated CMakeLists.txt

--- src/main/jasm/core/jasmbuild_info.h
+++ src/main/jasm/core/jasmbuild_info.h
@@ -1,3 +1,3 @@
 #define VERSION "0.0.1-develstage-release"
-#define BUILD_DATE_CORE "2016/03/12 23:43:28"
+#define BUILD_DATE_CORE "2016/03/14 21:29:26"
 #define COPYRIGHT "GNU Public License 3 - GPLv3"
