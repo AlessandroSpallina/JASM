82ff94f4b84223e7e9a5b591a09a4ca29d96f6ec
StefanoBelli <sa.belli@hotmail.it>
2016-03-19 18:33:06 +0100

    Update .gitignore

--- src/main/jasm/core/jasmbuild_info.h
+++ src/main/jasm/core/jasmbuild_info.h
@@ -1,3 +1,3 @@
 #define VERSION "0.0.1-develstage-release"
-#define BUILD_DATE_CORE "2016/03/19 17:01:50"
+#define BUILD_DATE_CORE "2016/03/19 18:26:04"
 #define COPYRIGHT "GNU Public License 3 - GPLv3"
