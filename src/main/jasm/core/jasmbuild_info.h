7bfee81af8c9ddcc4337b7e613c38763369943e7
StefanoBelli <sa.belli@hotmail.it>
2016-03-19 16:35:17 +0100

    sign-compare warnings suppressed

--- src/main/jasm/core/jasmbuild_info.h
+++ src/main/jasm/core/jasmbuild_info.h
@@ -1,3 +1,3 @@
 #define VERSION "0.0.1-develstage-release"
-#define BUILD_DATE_CORE "2016/03/19 13:58:08"
+#define BUILD_DATE_CORE "2016/03/19 16:33:37"
 #define COPYRIGHT "GNU Public License 3 - GPLv3"
