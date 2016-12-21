2abcad985e078a33e5461e1763f792a6cdc6441f
AlessandroSpallina <alessandrospallina1@gmail.com>
2016-12-21 23:36:33 +0100

    in test folder now there is a little example of dyn lib

---
+++ src/test/dynamic_modules/module.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void init()
+{
+  puts("Init Module..\n");
+}
+
+void routine()
+{
+    puts("This is ROUTINE\n");
+}
+
+void end()
+{
+  puts("Dealloc module resources..\n");
+}
