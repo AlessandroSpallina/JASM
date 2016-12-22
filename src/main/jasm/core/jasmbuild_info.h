849f62fabe84fc18cc1ce6a401516c7789135c4e
AlessandroSpallina <alessandrospallina1@gmail.com>
2016-12-22 12:31:12 +0100

    added pdf in doc

--- src/main/jasm/core/jasmbuild_info.h
+++ src/main/jasm/core/jasmbuild_info.h
@@ -1,27 +1,3 @@
-/****************************************************************************
-* Copyright © 2015 Alessandro Spallina
-* email: alessandrospallina1@gmail.com
-* github: https://github.com/AlessandroSpallina
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
-
-#ifndef _JASMBUILD_INFO_H
-#define _JASMBUILD_INFO_H
-
 #define VERSION "0.0.1-develstage"
-#define BUILD_DATE_CORE "2016/12/21 13:27:03"
+#define BUILD_DATE_CORE "2016/12/22 00:00:20"
 #define COPYRIGHT "GNU Public License 3 - GPLv3"
-
-#endif
